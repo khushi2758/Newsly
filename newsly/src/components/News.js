c22267239b316579d1d8d3243b1fcad27f461077
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2024-07-28 13:16:28 +0530

    Fetching API Key

--- newsly/src/components/News.js
+++ newsly/src/components/News.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        News is a Component
-        <NewsItems/>
+      <div className="container my-3">
+        <h2>NewsMonkey Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItems title= "myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItems title= "myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItems title= "myTitle" description="mydesc" />
+          </div>
+        </div>
       </div>
-      
-    )
+    );
   }
 }
 
-export default News
+export default News;
