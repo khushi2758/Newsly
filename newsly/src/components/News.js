2be16eee1e4b5156dc3f98ef82b9e673fdece85b
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2024-08-01 23:26:59 +0530

    Adding variable pageSize

--- newsly/src/components/News.js
+++ newsly/src/components/News.js
@@ -3,7 +3,7 @@ import NewsItems from "./NewsItems";
 
 export class News extends Component {
 
-  constructor(){
+  constructor(props){
     super();
     console.log("it is a constructer");
     this.state = {
@@ -14,15 +14,15 @@ export class News extends Component {
   }
  
     async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=Us&category=business&apiKey=082a76f337704f5f89ea3186f64eed5e&page=1"
+    let url = `https://newsapi.org/v2/top-headlines?country=Us&category=business&apiKey=082a76f337704f5f89ea3186f64eed5e&page=1pageSize=${this.props.pageSize}`
       let data = await fetch(url);
       let parsedData =await data.json()
       console.log(parsedData);
-   this.setState({articles: parsedData.articles})
+   this.setState({articles: parsedData.articles,totalResults: parsedData.totalResults})
     }
     handlePreviousClick= async ()=>{
       console.log("prev")
-      let url = `https://newsapi.org/v2/top-headlines?country=Us&category=business&apiKey=082a76f337704f5f89ea3186f64eed5e&page=${this.state.page - 1}`
+      let url = `https://newsapi.org/v2/top-headlines?country=Us&category=business&apiKey=082a76f337704f5f89ea3186f64eed5e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parsedData =await data.json()
       this.setState({
@@ -32,20 +32,24 @@ export class News extends Component {
 
         }
         handleNextClick= async () =>{
+          if(this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+
+          }
+         else{
           console.log("next")
-          let url = `https://newsapi.org/v2/top-headlines?country=Us&category=business&apiKey=082a76f337704f5f89ea3186f64eed5e&page=${this.state.page + 1}`
+          let url = `https://newsapi.org/v2/top-headlines?country=Us&category=business&apiKey=082a76f337704f5f89ea3186f64eed5e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
           let data = await fetch(url);
           let parsedData =await data.json()
           this.setState({
             page:this.state.page + 1,
             articles: parsedData.articles
-          })
+          })}
         }
   render() {
     console.log("render")
     return (
       <div className="container my-3">
-        <h1>Newsly - Top Headlines</h1>
+        <h1 className= "text-center py-7px">Newsly - Top Headlines</h1>
         <div className="row">
           {/*Looping through an array in JSX to display NewsItems from state---------------------------------------->>*/}
         {this.state.articles.map((element)=>{
@@ -57,7 +61,7 @@ export class News extends Component {
           <div className="container d-flex justify-content-between my-4">
           <button   disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> 	&larr;preview</button>
           {/*disable ==> {this.state.page>=1} // its mean if page is > or = 1 then the previous bottor will be disable--------------------------------- */}
-          <button   type="button" className="btn btn-dark" onClick={this.handleNextClick}>next	&rarr;</button>
+          <button disabled={this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)}  type="button" className="btn btn-dark" onClick={this.handleNextClick}>next	&rarr;</button>
           </div>
           </div>
         </div>
